626e5ae1306c9975b19e56c918a2d86bc3722218
Nick <njr48@cornell.edu>
2019-10-30 15:21:49 -0400

    compiling

--- types.ml
+++ types.ml
@@ -1,10 +1,13 @@
 open Yojson.Basic.Util
 
 type t = string
+exception UnknownType of t
 
-let rec hash f i= function
-  | [] -> f 
-  | h::t -> hash (fun x -> if x = h then i else f x) (i+1) t
+let hash lst = 
+  let rec create_hash f i = function
+    | [] -> f 
+    | h::t -> create_hash (fun x -> if x = h then i else f x) (i+1) t
+  in create_hash (fun x -> raise (UnknownType x)) 0 lst
 
 let json_data jdata = 
   jdata |> member "cols" |> to_list |> List.map to_float
@@ -21,5 +24,7 @@ let rec cols j m = function
 let type_matrix json =  
   let n = json |> member "size" |> to_int in 
   let m = Array.make_matrix n n 0.0 in 
-  let data_lst = json |> member "data" |>to_list|> List.map json_data in 
-  cols 0 m data_lst;
+  let json_data_lst = json |> member "data" |>to_list in 
+  let data = json_data_lst|> List.map json_data in 
+  let hash_fun = json_data_lst |> List.map to_string |> hash in 
+  (cols 0 m data,hash_fun)
--- types.mli
+++ types.mli
@@ -1,1 +1,12 @@
-type t
+
+(** [UnknownType t] is raised when a pokemon or move tries to use a move of 
+    a non-known type. *)
+exception UnknownType of string
+
+(** [hash lst] is a function that creates the hash function for converting types
+    of pokemon as string into integers. The returned function will 
+    raise [UnknownType t] when applied to [t] if that type is not valid. *)
+val make_hash: string list -> (string-> int)
+
+(** [type_matrix j] is TODO *)
+val type_matrix: Yojson.Basic.t -> float array array * (string -> int) 
