ef8cae4ccd5b2a7ae0884e5e4fc8b95ac2d4e876
Nick <njr48@cornell.edu>
2019-10-30 12:25:48 -0400

    beginning of implementation of type matrix

--- types.ml
+++ types.ml
@@ -1,3 +1,10 @@
+open Yojson.Basic.Util
+
 module type types = sig
   type t = Normal | Fire | Water | Grass | Electric | Ground 
 end
+
+let type_matrix json =  
+  let n = json |> member "size" |> to_int in 
+  let mat = Array.make_matrix n n 0.0 in 
+  for i=0 to n do 
